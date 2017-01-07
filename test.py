c13a8ba13c0a3dcf94f8bd059d29525894620f4e
wangnanphp <wangnanphp@163.com>
2017-01-07 22:49:25 +0800

    测试练习

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-while True:
-    for i in ["/", "-", '|', '\\', "|"]:
-        print "%s\r" % i,
+#while True:
+#    for i in ["/", "-", '|', '\\', "|"]:
+#        print "%s\r" % i,
+
+
+#print "How old are you?", raw_input()
+#print "How old are you? %s " % raw_input()
+print raw_input("How old are you? ")
