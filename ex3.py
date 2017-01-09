38829463772f46a0b78cb5c222b014ab0353af4a
wangnan <wangnanphp@163.com>
2017-01-09 09:48:48 +0830

    练习3

---
+++ ex3.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+# 输出一句话
+print "I will now count my chickens:"
+
+# 输出 30/6+25=30
+print "Hens", 25 + 30 / 6
+# 输出 100 - 25 * 3 % 4 = 97
+print "Roosters", 100 - 25 * 3 % 4
+
+# 输出一句话
+print "Now I will count the eggs:"
+
+# 输出: 7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# False
+print "Is it true 3 + 2 < 5 - 7?"
+
+# False
+print 3 + 2 < 5 - 7
+
+# 5
+print "What is 3 + 2?", 3 + 2
+# -2
+print "What is 5 - 7?", 5 - 7
+
+# 输出一句话
+print "Oh, that's why it's False."
+
+# 输出一句话
+print "How about same more."
+
+# Ture
+print "Is it greater?", 5 > -2
+# Ture
+print "Is it greater or equal?", 5 >= -2
+# False
+print "Is it less or equal?", 5 <= -2
+
+print 7.0 / 4.0
+print 7 / 4
